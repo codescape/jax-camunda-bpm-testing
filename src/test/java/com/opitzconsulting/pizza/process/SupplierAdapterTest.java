10e668c038e3cedb4bc1590b4ca6d6d6d41db8cb
Stefan Glase <stefan.glase@googlemail.com>
2014-06-28 14:17:58 +0200

    fix it ;)

--- src/test/java/com/opitzconsulting/pizza/process/SupplierAdapterTest.java
+++ src/test/java/com/opitzconsulting/pizza/process/SupplierAdapterTest.java
@@ -1,14 +1,18 @@
 package com.opitzconsulting.pizza.process;
 
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.cdi.BusinessProcess;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SupplierAdapterTest {
 
     private SupplierAdapter supplierAdapter;
@@ -16,10 +20,14 @@ public class SupplierAdapterTest {
     @Mock
     private BusinessProcess businessProcess;
 
+    @Mock
+    private RuntimeService runtimeService;
+
     @Before
     public void setUp() {
         supplierAdapter = new SupplierAdapter();
         supplierAdapter.businessProcess = businessProcess;
+        supplierAdapter.runtimeService = runtimeService;
     }
 
     @Test
